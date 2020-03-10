85b25dcb5f3f4a04db16b034e3010fd7d29982b7
marpusik <47226234+marpusik@users.noreply.github.com>
2020-03-10 21:45:51 +0300

    Add function remove()

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const box = document.querySelector('#mega')
 const position = {
-    x: -1, y: 0.5, z: -1.5,
+    x: -1,
+    y: 0.5,
+    z: -1.5,
 }
 console.log(box)
 window.addEventListener('keydown', (e) => {
-    console.log(e, position)
-    if (e.key == 8 && position.z < 4) { position.z++ }
-    if (e.key == 9 && position.z > -7) { position.z-- }
-    if (e.key == 6 && position.x < 6) { position.x++ }
-    if (e.key == 7 && position.x > -6) { position.x-- }
+    // console.log(e, position)
+    if (e.key == 8 && position.z < 4) {
+        position.z++
+    }
+    if (e.key == 9 && position.z > -7) {
+        position.z--
+    }
+    if (e.key == 6 && position.x < 6) {
+        position.x++
+    }
+    if (e.key == 7 && position.x > -6) {
+        position.x--
+    }
     box.setAttribute('position', position)
 })
 
@@ -24,63 +34,91 @@ const tetris = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 0, 0, 0, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
+
+tetris.reverse()
 
 function render() {
     tetris.map((row, y) => {
-        console.log(row)
+        // console.log(row)
         row.map((cell, x) => {
-            console.log(cell, x, y)
+            // console.log(cell, x, y)
             // if cell=1 add cube with position x,y
             var sceneEl = document.querySelector('a-scene');
             var newEl = document.createElement('a-box');
+            var position;
             if (cell == 1) {
                 newEl.setAttribute('color', 'red');
-                sceneEl.appendChild(newEl);
-                const position = { x, y, z: -10 }
-                newEl.setAttribute('position', position);
+                position = {
+                    x,
+                    y,
+                    z: -10
+                }
             } else {
                 newEl.setAttribute('color', 'green');
                 newEl.setAttribute('width', '.1')
                 newEl.setAttribute('height', '.1')
                 newEl.setAttribute('depth', '.1')
-                sceneEl.appendChild(newEl);
-                const position = { x, y, z: -10 }
-                newEl.setAttribute('position', position);
+                position = {
+                    x,
+                    y,
+                    z: -10
+                }
             }
-
+            sceneEl.appendChild(newEl);
+            newEl.classList.add('box')
+            newEl.setAttribute('position', position);
         })
 
     })
 }
 
-document.querySelector('a-scene').addEventListener('loaded', function () { render() })
+document.querySelector('a-scene').addEventListener('loaded', function () {
+    render();
+    setTimeout(() => {
+        tetris.shift()
+        tetris.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        remove('.box')
+        render()
+    }, 2000)
+})
 
 // game tik
 
-setInterval(() => {
-    console.log('tik')
-    tetris.pop()
-    tetris.shift([0,0,0,0,0,0,0,0,0,0])
-}, 2000)
+console.log(tetris.length)
+
+function remove(el) {
+    // console.log(tetris.length)
+    //tetris.splice(0, 30)
+    const all = document.querySelectorAll(el)
+    console.log(all)
+    all.forEach((element) => {
+        element.remove()
+    })
+}
+// setInterval(() => {
+//     console.log('tik')
+//     tetris.pop()
+//     tetris.shift([0,0,0,0,0,0,0,0,0,0])
+// }, 2000)
